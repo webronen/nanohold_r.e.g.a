40b56552506bb8ad84e71143c2c18cc06c9c9c73
Jarno Piironen <89977375+webronen@users.noreply.github.com>
2026-01-28 22:38:07 +0200

    Create main.h

---
+++ main.h
@@ -0,0 +1,70 @@
+#pragma once
+
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <Adafruit_TinyUSB.h>
+#include <vl53l4cd_class.h>
+#include <SCServo.h>
+
+#define HZ_TO_US(hz) (1000000UL / (hz))
+#define M_TO_US(m) ((m)*60 * 1000000UL)
+#define S_TO_US(s) ((s)*1000000UL)
+#define DISTANCE_LPF 0.75f
+
+#define LDO_ENABLE_PIN 13
+#define SERIAL_BAUDRATE_1M 1000000
+#define I2C_FREQUENCY_400K 400000
+#define I2C_CLK_PIN 36
+#define I2C_SDA_PIN 38
+
+#define AUTO_DISTANCE_MM 30
+
+#define GPIO_STATUS_PIN 11   // P0.11
+#define GPIO_RIGHT_BUTTON 0  // P1.00
+#define GPIO_LEFT_BUTTON 24  // P0.24
+
+#define SCS0009_DEFAULT_ID 1
+
+#define PRESS_UP_POSITION 0    // ?
+#define PRESS_DOWN_POSITION 0  // ?
+#define PRESS_LOAD_LIMIT 0     // ?
+
+#define PRESS_UP_SPEED 150
+#define PRESS_DOWN_SPEED 300
+
+VL53L4CD sensor(&Wire, -1);
+SCSCL servo;
+
+typedef enum {
+  MANUAL = 0,
+  AUTO = 1,
+} PressMode;
+
+typedef enum {
+  IDLE = 0,
+  UP = 1,
+  DOWN = 2,
+  RESET = 3
+} PressStep;
+
+typedef struct {
+  PressMode mode;
+  PressStep step;
+  PressStep buttons;
+  uint32_t time_us;
+  uint32_t idle_us;
+  bool open;
+  bool latch;
+} PressState;
+
+static PressState state = { .mode = AUTO };
+
+static void blink_status_leds(const uint32_t interval_us);
+static inline void handle_reset_state(void);
+static inline void prepare_active_state(void);
+static inline void handle_idle_state(void);
+static inline void handle_press_up(void);
+static inline void handle_press_down(void);
+
+#endif  // MAIN_H
