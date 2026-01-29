e1ca2085e43b6727d2ff5175c3f7b0cf63cd02cd
Jarno Piironen <89977375+webronen@users.noreply.github.com>
2026-01-29 17:02:25 +0200

    Update main.h

--- main.h
+++ main.h
@@ -12,7 +12,7 @@
 #define S_TO_US(s) ((s)*1000000UL)
 
 #define IDLE_TIMEOUT_M M_TO_US(1)
-#define SLEEP_TIMEOUT_M M_TO_US(5)
+#define SLEEP_TIMEOUT_M M_TO_US(2)
 
 #define LDO_ENABLE_PIN 13
 #define SERIAL_BAUDRATE_1M 1000000
