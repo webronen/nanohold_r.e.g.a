3533c3bf217fdd7110c043a13729904ef43bcb7f
Jarno Piironen <89977375+webronen@users.noreply.github.com>
2026-02-01 19:20:40 +0200

    Update main.h

--- main.h
+++ main.h
@@ -112,6 +112,6 @@ static inline void idle_shutdown(void);
 static void idle_disconnect_gpio(void);
 
 static inline void active_enable_power(void);
-static void inline active_blink_status(void);
+static inline void active_blink_status(void);
 
 #endif  // MAIN_H
